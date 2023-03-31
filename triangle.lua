40b6b5b4b4e042d09a445f959bf910741e11fca4
ElectricWildberry <114389107+ElectricWildberry@users.noreply.github.com>
2023-03-31 22:00:52 +0900

    Update triangle.lua

--- triangle.lua
+++ triangle.lua
@@ -49,14 +49,14 @@ end
 
 function Billboard:Order(rad) --this checks whether the object is within render distance, and if it's behind the camera
     self.rad = rad
-    self.rotz = (self.zpos - self.camera.zpos) * math.cos(rad) + (self.xpos - self.camera.xpos) * math.sin(rad) + self.camera.zpos
+    self.rotz = (self.zpos - self.camera.zpos) * math.cos(rad) + (self.xpos - self.camera.xpos) * math.sin(rad) + self.camera.zpos -- rotating the z point around the y axis
     return (self.rotz < self.camera.zpos) and ((self.rotz - self.camera.zpos) > -400 and (self.rotz - self.camera.zpos) < 0)
 end
 
 function Billboard:CheckZ(rad) -- function to check whether the object is within the drawspace
 
-    self.rotx = (self.xpos - self.camera.xpos) * math.cos(rad) - (self.zpos - self.camera.zpos) * math.sin(rad) + self.camera.xpos
-    self.drawx = -200 * (self.rotx - self.camera.xpos) / math.abs(self.rotz - self.camera.zpos)
+    self.rotx = (self.xpos - self.camera.xpos) * math.cos(rad) - (self.zpos - self.camera.zpos) * math.sin(rad) + self.camera.xpos -- rotating the x point around the y axis
+    self.drawx = -200 * (self.rotx - self.camera.xpos) / math.abs(self.rotz - self.camera.zpos) -- using perspective math to calculate draw point on draw plane
 
     if (self.drawx > 100) or (self.drawx < -100) then return false else return true end
     
@@ -68,12 +68,12 @@ function Billboard:ShowSelf()
     
     self.roty = self.ypos
 
-    self.drawy = 200 * (self.roty - self.camera.ypos) / math.abs(self.rotz - self.camera.zpos)
+    self.drawy = 200 * (self.roty - self.camera.ypos) / math.abs(self.rotz - self.camera.zpos) -- using perspective math to calculate draw point on draw plane
     self.drawz = self.rotz
-    self.drawSizeX = (200 * (self.rotx + self.w - self.camera.xpos) / (self.rotz - self.camera.zpos)) - self.drawx
+    self.drawSizeX = (200 * (self.rotx + self.w - self.camera.xpos) / (self.rotz - self.camera.zpos)) - self.drawx -- using perspective math to calculate draw point on draw plane
     self.drawSizeY = (200 * (self.roty + self.h - self.camera.ypos) / math.abs(self.rotz - self.camera.zpos)) - self.drawy
     
-    self.s = self.drawSizeX / self.w
+    self.s = self.drawSizeX / self.w -- calculating how big the sprite is compared to the original
 
     self.img:drawScaled(self.drawx + 100 + self.drawSizeX/2, self.drawy + 60 - self.drawSizeY, -self.s)
     
