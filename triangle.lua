db6c7c3a0a3e235fb3671ef64164311fd2ad6baa
ElectricWildberry <114389107+ElectricWildberry@users.noreply.github.com>
2023-03-31 21:59:14 +0900

    Update triangle.lua

--- triangle.lua
+++ triangle.lua
@@ -4,83 +4,70 @@ local gfx <const> = pd.graphics
 import "CoreLibs/crank"
 import "CoreLibs/animator"
 
-class('Triangle').extends(gfx.sprite)
+class('Billboard').extends(gfx.sprite)
 
-function Triangle:init(x, y, z, w, h, camera, img)
+function Billboard:init(x, y, z, w, h, camera, img)
 
-    self.disabled = false
+    self.img = img --this is needed to show an image
 
-    self.img = img
-
-    self.xpos = x
-    self.ypos = -y
+    self.xpos = x 
+    self.ypos = -y --y is negative due to how the playdate sprite coordinate system works
     self.zpos = z
 
-    self.w, self.h = self.img:getSize()
+    self.w, self.h = self.img:getSize() -- this only happens once (for computing's sake), which is why all animation sprites need to be the same size
 
-    self.camera = renderer.camera
+    self.camera = renderer.camera -- reference to the camera (to get position and rotation)
     self.rad = 0
-    self.pitch = 0
+    self.pitch = 0 -- unused
     self.rot = 0
-    self.piv = 0
+    self.piv = 0 -- unused
 
-    self.drawx = 0
-    self.drawy = 0
+    self.drawx = 0 
+    self.drawy = 0 --draw coordinates, drawz is for draw order
     self.drawz = 0
 
     self.drawSizeX = 0
     self.drawSizeY = 0
 
-    self.s = 0
+    self.s = 0 -- temporary variable for the draw scale (self.scale is unwritable)
 
     self.rotx = 0
-    self.roty = 0
+    self.roty = 0 -- rotated positions, calculated from camera position and rotation
     self.rotz = 0
 
-    self.pic = nil
-
-    table.insert(renderer.tri_queue, self)
+    table.insert(renderer.tri_queue, self) --inserts self into the global renderer
 
     --self:add()
     
 end
 
-function Triangle:update()
+function Billboard:update()
 
     --self:ShowSelf()
 
 end
 
-function Triangle:Order(rad)
+function Billboard:Order(rad) --this checks whether the object is within render distance, and if it's behind the camera
     self.rad = rad
     self.rotz = (self.zpos - self.camera.zpos) * math.cos(rad) + (self.xpos - self.camera.xpos) * math.sin(rad) + self.camera.zpos
     return (self.rotz < self.camera.zpos) and ((self.rotz - self.camera.zpos) > -400 and (self.rotz - self.camera.zpos) < 0)
 end
 
-function Triangle:CheckZ(rad)
-
-    --if self.disabled then return false end
+function Billboard:CheckZ(rad) -- function to check whether the object is within the drawspace
 
     self.rotx = (self.xpos - self.camera.xpos) * math.cos(rad) - (self.zpos - self.camera.zpos) * math.sin(rad) + self.camera.xpos
-    --self.rotz = (self.zpos - self.camera.zpos) * math.cos(rad) + (self.xpos - self.camera.xpos) * math.sin(rad) + self.camera.zpos
     self.drawx = -200 * (self.rotx - self.camera.xpos) / math.abs(self.rotz - self.camera.zpos)
 
     if (self.drawx > 100) or (self.drawx < -100) then return false else return true end
-
-    --return true
-
-    --return (self.rotz < self.camera.zpos) and (math.abs(self.rotz - self.camera.zpos) < 300)
+    
 end
 
-function Triangle:ShowSelf()
+function Billboard:ShowSelf()
 
-    if not self:CheckZ(self.rad) then return end
-
-    --self.rotx = (self.xpos - self.camera.xpos) * math.cos(rad) - (self.zpos - self.camera.zpos) * math.sin(rad) + self.camera.xpos
+    if not self:CheckZ(self.rad) then return end -- this saves a substantial amount of time
+    
     self.roty = self.ypos
-    --self.rotz = (self.zpos - self.camera.zpos) * math.cos(rad) + (self.xpos - self.camera.xpos) * math.sin(rad) + self.camera.zpos
 
-    --self.drawx = -400 * (self.rotx - self.camera.xpos) / math.abs(self.rotz - self.camera.zpos)
     self.drawy = 200 * (self.roty - self.camera.ypos) / math.abs(self.rotz - self.camera.zpos)
     self.drawz = self.rotz
     self.drawSizeX = (200 * (self.rotx + self.w - self.camera.xpos) / (self.rotz - self.camera.zpos)) - self.drawx
